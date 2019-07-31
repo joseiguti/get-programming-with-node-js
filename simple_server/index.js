98b172185e907425e2a51b692d15ab67ff48c9cc
José Ignacio Gutiérrez Guzmán <joselitosinmiedo@gmail.com>
2019-07-31 09:04:54 -0500

    Ejercicios

---
+++ simple_server/index.js
@@ -0,0 +1,27 @@
+
+const port = 3000, 
+
+http = require("http"), // Require the http and http-status-codes modules. 
+
+httpStatus = require("http-status-codes"),
+
+app = http.createServer((request, response) => { // Create the server with request and response parameters.
+    
+    console.log("Received an incoming request!");
+    
+    response.writeHead(httpStatus.OK, {
+        "Content-Type": "text/html"
+    }); // Create the server with request and response parameters.
+
+    let responseMessage = "<h1>Hello, Universe!</h1>";
+
+    response.write(responseMessage);
+
+    response.end();
+
+    console.log(`Sent a response : ${responseMessage}`);
+});
+
+app.listen(port); // Tell the application server to listen on port 3000.
+
+console.log(`The server has started and is listening on port number:${port}`);
